31a65166b76de0d1987ac648dcf7a847c45b4d4e
DOUSSELIN Aurèle <aurele.dousselin@sword-group.com>
2018-09-06 15:23:53 +0200

    grid generator

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour {
+
+    public int width = 100;
+    public int height = 100;
+
+    public GameObject[] outerWallTiles;
+    public GameObject[] groundTiles;
+
+    private Transform boardHolder;
+
+    void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+        for (int i = -1; i < width + 1; i++)
+        {
+            for (int j = -1; j < height + 1; j++)
+            {
+                GameObject toInstanciate = groundTiles[0];
+                if (i == -1 || i == width || j == -1 || j == height)
+                {
+                    toInstanciate = outerWallTiles[0];
+                }
+                GameObject instance = Instantiate(toInstanciate, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    public void SetupScene()
+    {
+        BoardSetup();
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [HideInInspector]public BoardManager boardScript;
+    
+	void Awake () {
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+	}
+
+    void InitGame()
+    {
+        boardScript.SetupScene();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
