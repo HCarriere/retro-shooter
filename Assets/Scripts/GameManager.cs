85bb116a1c626ee388bee2852c149056cb3db133
DOUSSELIN Aurèle <aurele.dousselin@sword-group.com>
2018-09-10 14:21:24 +0200

    menu : play, resume, exit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,23 +1,69 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
     [HideInInspector]public BoardManager boardScript;
-    
-	void Awake () {
+
+    private GameObject menuImage;
+    private Button playButton;
+    private Button resumeButton;
+
+    private bool pause = true;
+
+    void Awake () {
         boardScript = GetComponent<BoardManager>();
-        InitGame();
+        menuImage = GameObject.Find("Menu");
+        playButton = GameObject.Find("Play").GetComponent<Button>();
+        resumeButton = GameObject.Find("Resume").GetComponent<Button>();
 	}
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        pause = true;
+        menuImage.SetActive(true);
+    }
+
+    public void Play()
+    {
+        menuImage.SetActive(false);
+        playButton.interactable = false;
+        resumeButton.interactable = true;
+        InitGame();
+    }
+
+    public void Resume()
+    {
+        menuImage.SetActive(false);
+        pause = false;
+        Time.timeScale = 1;
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 
     void InitGame()
     {
         boardScript.SetupScene();
+        pause = false;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyDown(KeyCode.Escape) && !pause)
+        {
+            Pause();
+        }
 	}
 }
